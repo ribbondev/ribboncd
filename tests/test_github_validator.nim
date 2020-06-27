2d5f20ab9986451c6c5ada7e0d4e67c7c2b25d6f
lilah <xxami@users.noreply.github.com>
2020-06-27 16:09:25 +0100

    add github signature tests

---
+++ tests/test_github_validator.nim
@@ -0,0 +1,30 @@
+import unittest
+import github_validator
+
+suite "github_validator.constant_cmp":
+
+  test "returns true when strings of varying lengths are equal":
+    check(constant_cmp("", "") == true)
+    check(constant_cmp("a", "a") == true)
+    check(constant_cmp("-", "-") == true)
+    check(constant_cmp("ab", "ab") == true)
+    check(constant_cmp("abcd123", "abcd123") == true)
+    check(constant_cmp("abcd 123 xyz /*+-", "abcd 123 xyz /*+-") == true)
+
+  test "returns false when strings of verying lengths are not equal":
+    check(constant_cmp("a", "") == false)
+    check(constant_cmp("", "a") == false)
+    check(constant_cmp("a", "b") == false)
+    check(constant_cmp("b", "a") == false)
+    check(constant_cmp("ab", "ac") == false)
+    check(constant_cmp("ac", "ab") == false)
+    check(constant_cmp("abc+def", "abc-def") == false)
+    check(constant_cmp("abc def 123", "abc def 1234") == false)
+    check(constant_cmp("abc def 1234", "abc def 123") == false)
+
+suite "github_validator.validate_secret":
+  test "returns true when github hash is hmac of secret and body":
+    let secret = "secret123"
+    let body = "{ x: 1, y: 1 }"
+    let github_sig = "sha1=1C30FB84424FD7B175E64EDD94148CD9E2D58B97"
+    check(validate_secret(secret, body, github_sig) == true)
